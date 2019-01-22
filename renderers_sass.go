95c02cff5400859f442eaac2047d196001b7f4b3
Gary Hetzel <garyhetzel@gmail.com>
2019-01-22 18:17:15 -0500

    Working on funcdocs

--- renderers_sass.go
+++ renderers_sass.go
@@ -17,7 +17,7 @@ func (self *SassRenderer) ShouldPrerender() bool {
 func (self *SassRenderer) Render(w http.ResponseWriter, req *http.Request, options RenderOptions) error {
 	defer options.Input.Close()
 
-	if sass, err := libsass.New(w, options.Input); err == nil {
+	if sass, err := libsass.New(w, options.Input, libsass.OutputStyle(libsass.EXPANDED_STYLE)); err == nil {
 		w.Header().Set(`Content-Type`, `text/css; charset=utf-8`)
 
 		return sass.Run()
