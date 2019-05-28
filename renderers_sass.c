3c59dcf6e288491d7c4ef704fcdee6c71eefd0fa
Gary Hetzel <garyhetzel@gmail.com>
2019-05-28 15:45:24 -0400

    Remove backwards-incompat libsass call

--- renderers_sass.c
+++ renderers_sass.c
@@ -10,11 +10,11 @@ Sass_Import_List diecast_sass_importer(const char *url, Sass_Importer_Entry cb, 
     // get the file contents
     if (go_retrievePath(cookie, url, &data) >= 0) {
         // success, return the data
-        list[0] = sass_make_import_entry(url, sass_copy_c_string(data), 0);
+        list[0] = sass_make_import_entry(url, data, 0);
     } else {
         // error, return the message
         list[0] = sass_make_import_entry(url, 0, 0);
-        sass_import_set_error(list[0], sass_copy_c_string(data), 0, 0);
+        sass_import_set_error(list[0], data, 0, 0);
     }
 
     return list;
