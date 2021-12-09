9115eefaf14e7568cc49cba401f2564a67ca84e2
Gary Hetzel <its@gary.cool>
2021-12-09 13:57:06 -0500

    bump stockutil

--- functions_builtin.go
+++ functions_builtin.go
@@ -1,5 +1,21 @@
 package diecast
 
-func builtinFunctionHandler(req *SandboxMessage) *SandboxMessage {
+import (
+	"github.com/ghetzel/go-stockutil/maputil"
+)
 
+type InProcessRunner struct {
+	funcs maputil.Map
+}
+
+func (self *InProcessRunner) HandleMessage(req *SandboxMessage) *SandboxMessage {
+	defer req.DoneResponding()
+
+	if fn := self.funcs.Get(req.Command); !fn.IsNil() {
+
+	} else {
+		req.Missing = true
+	}
+
+	return req
 }
--- sandbox.go
+++ sandbox.go
@@ -13,6 +13,7 @@ type SandboxMessage struct {
 	Command      string        `json:"command"`
 	Args         []interface{} `json:"args"`
 	Response     interface{}   `json:"response"`
+	Missing      bool          `json:"missing"`
 	ErrorMessage string        `json:"error"`
 	RequestedAt  time.Time     `json:"requested_at"`
 	RespondedAt  time.Time     `json:"responded_at"`
@@ -22,8 +23,21 @@ func (self *SandboxMessage) Error() string {
 	return self.ErrorMessage
 }
 
+func (self *SandboxMessage) SetError(message error) {
+	if message == nil {
+		self.ErrorMessage = ``
+	} else {
+		self.ErrorMessage = message.Error()
+	}
+}
+
 func (self *SandboxMessage) HasResponded() bool {
 	return self.RespondedAt.IsZero()
+}
+
+func (self *SandboxMessage) DoneResponding() time.Duration {
+	self.RespondedAt = time.Now()
+	return self.Duration()
 }
 
 func (self *SandboxMessage) Duration() time.Duration {
